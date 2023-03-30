2d04fad3af45ab67da0b1674d57786e98d465f4c
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-03-30 15:56:54 -0300

    track solving time of LP model

--- get_ef_objs.py
+++ get_ef_objs.py
@@ -1,5 +1,6 @@
 import pickle
 from pathlib import Path
+from time import time
 import numpy as np
 from tqdm import tqdm
 
@@ -14,6 +15,7 @@ if __name__ == '__main__':
 
     ef_objs = dict()
 
+    times = list()
     for well_fp in tqdm(list(wells_fps)):
         with open(well_fp, 'rb') as f:
             well = pickle.load(f)
@@ -31,20 +33,30 @@ if __name__ == '__main__':
                 ef_objs.pop(well_name, None)
                 break
 
+            possible_fixes = list()
             for i in range(len(C) - 1):
                 for j in range(len(GL) - 1):
                     cs_to_fix = [C[i], C[i+1]]
                     gls_to_fix = [GL[j], GL[j+1]]
+                    possible_fixes.append((cs_to_fix, gls_to_fix))
 
+            selected_fixes = [possible_fixes[i] for i in np.random.choice(len(possible_fixes), len(possible_fixes) // 2, replace=False)]
+            for cs_to_fix, gls_to_fix in selected_fixes:
                     fixed_model = fix_c_gl(model, cs_to_fix, gls_to_fix)
                     fixed_model.setParam("TimeLimit", 300)
                     fixed_model.update()
+
+                    start = time()
                     fixed_model.optimize()
+                    times.append(time() - start)
 
                     if fixed_model.status != 2:
                         ef_objs[well_name][tuple(cs_to_fix+gls_to_fix+[q_gl_max,])] = -1
                     else:
                         ef_objs[well_name][tuple(cs_to_fix+gls_to_fix+[q_gl_max,])] = fixed_model.ObjVal
 
-    with open('ef_objs.pkl', 'wb') as f:
-        pickle.dump(ef_objs, f)
+    print('Total running time = ', sum(times))
+    print('Average running time = ', sum(times) / len(times))
+
+    # with open('reduced_ef_objs.pkl', 'wb') as f:
+    #     pickle.dump(ef_objs, f)
